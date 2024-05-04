544c562eff3b6ac18e8cda7eedf1a2a164c704b0
Stepan72 <st.nim@list.ru>
2024-05-04 06:43:34 +0500

    Aora started v0.01

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function App() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <StatusBar style="auto" />
+      <Text className="text-3xl font-pblack">Aora!</Text>
+      <Link href="/profile">Go to Profile</Link>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
