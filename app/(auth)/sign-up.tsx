69b68830b121e857456199c27a045dc4a2e0f0a0
Stepan72 <st.nim@list.ru>
2024-05-08 16:27:22 +0500

    Added appwrite sign up logic v0.06

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,16 +1,33 @@
-import { View, Text, ScrollView, Image } from "react-native";
+import { View, Text, ScrollView, Image, Alert } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
+import { createUser } from "../../lib/appwrite";
 
 const SignUp = () => {
   const [form, setForm] = useState({ username: "", email: "", password: "" });
   const [isSubbmitting, setIsSubbmitting] = useState(false);
 
-  const submit = () => {};
+  const submit = async () => {
+    if (!form.email || !form.password || !form.username) {
+      Alert.alert("Error", "Please fill all the fields!");
+    }
+    setIsSubbmitting(true);
+
+    try {
+      const result = await createUser(form.email, form.password, form.username);
+
+      /// set it to global state ...
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", `${error}`);
+    } finally {
+      setIsSubbmitting(false);
+    }
+  };
 
   return (
     <SafeAreaView className="bg-primary h-full py-2">
